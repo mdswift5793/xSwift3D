b21ee93bbd5fe17f02b6f56952479d551c435c62
swifty705 <mdswift5793@gmail.com>
2022-05-06 16:35:41 -0400

    Initial Commit

---
+++ command.h
@@ -0,0 +1,28 @@
+#ifndef COMMAND_H
+#define COMMAND_H 0x0001
+#include "game_actor.h"
+
+void jump(game_actor *a){
+    a->move(UP);
+}
+void move_left(game_actor *a){
+    a->move(LEFT);
+}
+void move_right(game_actor *a){
+    a->move(RIGHT);
+}
+void crouch(game_actor *a){
+    a->move(DOWN);
+}
+
+typedef void (*execute)(game_actor *a);
+typedef struct command {
+    execute execute;
+} command;
+
+command JUMP {jump};
+command MOVE_LEFT {move_left};
+command MOVE_RIGHT {move_right};
+command CROUCH {crouch};
+
+#endif
---
+++ gameactor.h
@@ -0,0 +1,27 @@
+#ifndef GAMEACTOR_H
+#define GAMEACTOR_H 0x0003
+#define MAX_NUM 500
+
+
+struct game_actor *player;
+
+typedef void (*move)(vector v);
+typedef struct game_actor {
+    char** tag;
+    vector world_position;
+    move move;
+} game_actor;
+
+void init(){
+    player = malloc(sizeof(game_actor));
+}
+
+game_actor set_actor(char** id){
+    player->tag == id;
+}
+
+game_actor *get_actor(){
+    return *player;
+}
+
+#endif
